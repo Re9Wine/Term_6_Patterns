a9dbfa56425c878abe3be7e4841ddc9538c2ecd9
Re9Wine <asckortan@gmail.com>
2023-03-15 15:38:50 +0300

    Update Main.java

--- Patterns_Lab_2/src/main/java/org/example/Main.java
+++ Patterns_Lab_2/src/main/java/org/example/Main.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.example.Comparators.Patient.PatientDiagnosisComparator;
+import org.example.Comparators.Patient.PatientPhoneNumberComparator;
+import org.example.Entity.Patient;
 import org.example.Views.PatientView;
+
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
